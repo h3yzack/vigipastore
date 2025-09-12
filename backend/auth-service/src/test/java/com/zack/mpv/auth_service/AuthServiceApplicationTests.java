36207a3df63635317e6fd4c0a84ee24ad3752763
h3yzack <zuhaimia@gmail.com>
2025-09-12 12:45:44 +0000

    Initial project setup

---
+++ backend/auth-service/src/test/java/com/zack/mpv/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zack.mpv.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
